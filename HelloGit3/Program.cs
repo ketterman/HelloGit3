4c590add0dd7869aa9c236ee514f97be353e072f
Brian Ketterman <brian.ketterman@gmail.com>
2017-09-15 14:13:01 -0400

    Add project files.

---
+++ HelloGit3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGit3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloGit3");
+            Console.ReadKey(); 
+        }
+    }
+}
